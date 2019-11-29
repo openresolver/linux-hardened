d0e4868b4a6c422ef4be575b528b06390c92ddd5
Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
2019-11-29 16:27:14 +0100

    slub: Extend init_on_alloc to slab caches with constructors
    This has required some rework during the port to 5.13, due to
    da844b787245 ("kasan, mm: integrate slab init_on_alloc with HW_TAGS"),
    and the patch is actually quite simpler now since we do not need to
    unpoison objects anymore.
    
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    Signed-off-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    [nicolas.bouchinet@ssi.gouv.fr: pre/post-alloc hooks moved from mm/slab.h to mm/slub.c (see 6011be59910fb12b7)]
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>

--- mm/slub.c
+++ mm/slub.c
@@ -4225,6 +4225,8 @@ bool slab_post_alloc_hook(struct kmem_cache *s, struct list_lru *lru,
 		if (p[i] && init && (!kasan_init ||
 				     !kasan_has_integrated_init()))
 			memset(p[i], 0, zero_size);
+		if (p[i] && init && s->ctor)
+			s->ctor(p[i]);
 		kmemleak_alloc_recursive(p[i], s->object_size, 1,
 					 s->flags, init_flags);
 		kmsan_slab_alloc(s, p[i], init_flags);
